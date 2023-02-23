6de0dc536ecdc7da87e37694a3de2c6427669fbc
Chinwe <chinwe.nworgu@gmail.com>
2023-02-23 15:58:56 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,10 +11,13 @@ void more_numbers(void)
 
 	for (i = 1; i <= 10; i++)
 	{
-		if (j >= 10)
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
 			_putchar('1');
 			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
 
