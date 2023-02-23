8cb67217d4907c7f567b7a22e369ffdd0d97958d
Chinwe <chinwe.nworgu@gmail.com>
2023-02-23 14:01:37 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - print 0123456789
+ * @c: char to be checked
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
