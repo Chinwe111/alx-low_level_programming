e3d610198fe8a936acd16bbdaa017ab1854af78d
Chinwe <chinwe.nworgu@gmail.com>
2023-03-01 12:49:40 -0800

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncat - concatenate two strings
+ * @src: source of sting
+ * @dest: destination of string
+ * @n: length of string
+ * Return: string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		continue;
+	}
+	for (j = 0 src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] '\0';
+	return (dest);
+}
