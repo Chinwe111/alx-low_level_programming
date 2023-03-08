a562903c1d9f5376191023b47fcd907c808e4023
Chinwe <chinwe.nworgu@gmail.com>
2023-03-08 15:12:53 -0800

    100-is_palindrome.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,6 +12,9 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int check(int a, int b);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+int pal_checker(char *s, int i, int j);
 
 
 #endif
