fa6a40bb149ea51f43f667a5926d152251cbfa9e
Chinwe <chinwe.nworgu@gmail.com>
2023-03-23 08:18:36 -0700

    1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "variadic_function.h"
+/**
+ * print_numbers - prints numbers followed by a new line
+ * @n: number of parameter
+ * @separator: separator between two numbers
+ * Return: 0
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int x;
+	va_list nl;
+
+	va_start(nl, n);
+
+	if (separator == NULL)
+		separator = "";
+
+	for (x = 0; x < n; x++)
+	{
+		printf("%d", va_arg(nl, int));
+		if (x < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(nl);
+}
