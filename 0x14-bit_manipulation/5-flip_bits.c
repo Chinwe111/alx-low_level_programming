8fc37ead2079a4b80eede832841d6c7aff486ff2
Chinwe111 <chinwe.nworgu@gmail.com>
2023-04-08 15:00:37 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,18 +12,18 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int div, current;
-	int x, y;
+	unsigned int x, y;
 
-	y = 0;
+	x = 0;
 	current = 1;
 	div = n ^ m;
 
-	for (x = 0; x < (sizeof(unsigned long int) * 8); x++)
+	for (y = 0; y < (sizeof(unsigned long int) * 8); y++)
 	{
 		if (current == (div & current))
-			y++;
+			x++;
 		current <<= 1;
 	}
 
-	return (y);
+	return (x);
 }
